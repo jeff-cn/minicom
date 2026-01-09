f37839ca64301ad3188566bdf1a4fd556c548ab9
Jeff Huang <jeffh@live.cn>
2026-01-09 19:49:36 +0800

    Change EXTERN declarations to extern for variables

--- src/minicom.h
+++ src/minicom.h
@@ -109,13 +109,19 @@ EXTERN char *dial_tty;     /* tty to use. */
 
 EXTERN char *dial_name;	    /* System we're conneced to */
 EXTERN char *dial_number;   /* Number we've dialed. */
-EXTERN char *dial_user;     /* Our username there */
-EXTERN char *dial_pass;     /* Our password */
+//EXTERN char *dial_user;     /* Our username there */
+//EXTERN char *dial_pass;     /* Our password */
+
+extern char *dial_user;     /* Our username there */
+extern char *dial_pass;     /* Our password */
 
 #ifdef USE_SOCKET
-EXTERN int portfd_is_socket;	/* File descriptor is a unix socket */
-EXTERN int portfd_is_connected;	/* 1 if the socket is connected */
-EXTERN struct sockaddr_un portfd_sock_addr;	/* the unix socket address */
+//EXTERN int portfd_is_socket;	/* File descriptor is a unix socket */
+//EXTERN int portfd_is_connected;	/* 1 if the socket is connected */
+//EXTERN struct sockaddr_un portfd_sock_addr;	/* the unix socket address */
+extern int portfd_is_socket;    /* File descriptor is a unix socket */
+extern int portfd_is_connected; /* 1 if the socket is connected */
+extern struct sockaddr_un portfd_sock_addr;     /* the unix socket address */
 #define portfd_connected ((portfd_is_socket && !portfd_is_connected) \
                            ? -1 : portfd)
 #else
@@ -141,7 +147,8 @@ EXTERN int sbcolor;     /* Status Bar Background Color */
 EXTERN int st_attr;	/* Status Bar attributes. */
 
 /* jl 04.09.97 conversion tables */
-EXTERN unsigned char vt_outmap[256], vt_inmap[256];
+//EXTERN unsigned char vt_outmap[256], vt_inmap[256];
+extern unsigned char vt_outmap[256], vt_inmap[256];
 
 /* MARK updated 02/17/95 - history buffer */
 EXTERN int num_hist_lines;  /* History buffer size */
